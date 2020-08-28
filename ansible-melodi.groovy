d95c96fcdbc7ef91f99e562f86df23f5b2a8e906
aia89 <aiadosbaeva@gmail.com>
2020-08-27 19:11:34 -0500

    melodi

--- ansible-melodi.groovy
+++ ansible-melodi.groovy
@@ -1,22 +1,19 @@
 properties([
     parameters([
-        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true),
+        string(defaultValue: '', description: 'Please enter branch name', name: 'branch', trim: true)
         ])
     ])
 if (nodeIP?.trim()) {
     node {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            stage('Init') {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install epel-release -y'
+            stage('Pull Repo') {
+                git branch: '${branch}', changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-melodi.git'
             }
-            stage("Install git") {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install git -y'
-            }
-            stage("Install Java"){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
-            }
-            stage("Install Ansible"){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install ansible -y'
+            withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+                stage("Install Apache"){
+                    sh 'ansible-playbook -i "104.131.110.132," --private-key $SSHKEY main.yml'
+                }
             }
         }
     }
