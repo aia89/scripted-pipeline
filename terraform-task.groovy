fd1180fb018cbdd0bd4ee0d0edb9a86ee763ac1f
Aia Dos <32306452+aia89@users.noreply.github.com>
2020-09-01 18:32:29 -0500

    Update terraform-task.groovy

--- terraform-task.groovy
+++ terraform-task.groovy
@@ -6,7 +6,7 @@ properties([
 ])
 node{
     stage("Pull Repo"){
-        git url: 'https://github.com/ikambarov/terraform-vpc.git'
+        git url: 'https://github.com/aia89/terraform-vpc.git'
     }
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
         stage("Terrraform Init"){
